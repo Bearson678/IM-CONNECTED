d22f4a726ffeefc554805151630c9f1175a73b82
Chadrick-Liang <chadrickliang@gmail.com>
2025-08-17 19:10:07 +0800

    verified UC6 & UC7 end to end testing outcome

--- cypress/e2e/message_chatbot_summarize.cy.js
+++ cypress/e2e/message_chatbot_summarize.cy.js
@@ -1,5 +1,14 @@
 describe('chatbot use case 6', () => {
-    it('use case 6: summarize post', function() {
+
+  beforeEach(() => {
+    // Spy only, pass-through to real server
+    cy.intercept('GET', 'http://localhost:5001/api/v1/user/threadId').as('requestThreadId');
+    cy.intercept('POST', 'http://localhost:3000/api/assistants/threads/thread_pZoZ1n1B5b2zDgO0itJrksB3/messages').as('postMessage');
+    cy.intercept('POST', 'http://localhost:3000/api/assistants/threads/thread_pZoZ1n1B5b2zDgO0itJrksB3/actions').as('postAction');
+  });
+
+
+  it('use case 6: summarize post', function () {
     cy.visit('http://localhost:5173/');
     cy.get('[href="/login"] > .top-button').click();
     cy.get(':nth-child(2) > .form-input').clear('R');
@@ -7,9 +16,25 @@ describe('chatbot use case 6', () => {
     cy.get(':nth-child(3) > .form-input').clear('P');
     cy.get(':nth-child(3) > .form-input').type('Password!');
     cy.get('.login-button').click();
+    cy.wait(2000);
     cy.get('.icons > :nth-child(4)').click();
+    cy.wait(2000);
     cy.get('.input').clear('S');
     cy.get('.input').type('Summarize this post for me \'Extreme Stress from being a caregiver, i want to live separately.\'');
     cy.get('.button > img').click();
+
+    cy.wait('@postMessage').its('response.statusCode').should('eq', 200);
+    cy.wait('@requestThreadId').its('response.statusCode').should(
+      (code) => {
+      // 304 (cached) response
+      expect([200, 304]).to.include(code);
+    });
+    cy.wait('@postAction').its('response.statusCode').should('eq', 200);
+    
+
+    cy.wait(6000);
+    cy.get('div.assistantBubble').should('exist');
+    cy.get('div.userBubble').should('exist');
+    cy.contains("Summarize this post for me \'Extreme Stress from being a caregiver, i want to live separately.\'").should('be.visible'); 
   });
 })
--- cypress/e2e/message_chatbot_translate.cy.js
+++ cypress/e2e/message_chatbot_translate.cy.js
@@ -1,5 +1,12 @@
-describe('chatbot use case 7', () => { 
-    it('use case 7: translate foreign text', () => {
+describe('chatbot use case 7', () => {
+
+  beforeEach(() => {
+    // Spy only, pass-through to real server
+    cy.intercept('GET', 'http://localhost:5001/api/v1/user/threadId').as('requestThreadId');
+    cy.intercept('POST', 'http://localhost:3000/api/assistants/threads/thread_pZoZ1n1B5b2zDgO0itJrksB3/messages').as('postMessage');
+  });
+
+  it('use case 7: translate foreign text', () => {
     cy.visit('http://localhost:5173/');
     cy.get('[href="/login"] > .top-button').click();
     cy.get(':nth-child(2) > .form-input').clear('R');
@@ -7,9 +14,23 @@ describe('chatbot use case 7', () => {
     cy.get(':nth-child(3) > .form-input').clear('P');
     cy.get(':nth-child(3) > .form-input').type('Password!');
     cy.get('.login-button').click();
+    cy.wait(2000);
     cy.get(':nth-child(4) > .applicationIcon').click();
+    cy.wait(2000);
     cy.get('.input').clear('T');
     cy.get('.input').type('Translate this to english \'Hola\'.');
     cy.get('.button > img').click();
+
+    cy.wait('@postMessage').its('response.statusCode').should('eq', 200);
+    cy.wait('@requestThreadId').its('response.statusCode').should(
+      (code) => {
+      // 304 (cached) response
+      expect([200, 304]).to.include(code);
+    });
+
+    cy.wait(6000);
+    cy.get('div.assistantBubble').should('exist');
+    cy.get('div.userBubble').should('exist');
+    cy.contains("Translate this to english 'Hola'.").should('be.visible'); 
   });
 })
